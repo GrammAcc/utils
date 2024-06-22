ebcc4f425bfd3c0e955146aefae93d2b0d036e69
Dalton Lang <35282898+GrammAcc@users.noreply.github.com>
2024-06-21 20:04:22 -0500

    Add Dynamic DOM Loading Helpers
    I added a few helpers for dynamically populating the DOM to the
    javascript utils file.
    
    I've been using these helpers for the common task of adding a tree of
    nodes to a specific place in the layout or replacing a specific section
    with some other dynamic content.
    
    I also included a version of each helper that adds a loading spinner
    while the dynamic content is loaded. I don't usually use these in my
    sites since I prefer to diagnose the bottleneck and make the page load
    fast enough that it isn't noticeable, but there are some situations
    where having a closure like this that can automatically set up a loading
    spinner is useful, so I have included these variants in the utils file
    for future copy/pasta when I need them.

--- utils.mjs
+++ utils.mjs
@@ -1,12 +1,122 @@
-/** Bind some JS code to run on page load.
-  * @param {function} callback A 0-arity function to call once the page content is loaded.
+/** @type {string} */
+const SPINNER_IMAGE_URL = "/path/to/spinner/image";
+
+/** Replace child node tree of `parent` with `Element` returned by `cb`.
+  * Adds a loading spinner in place of the dynamic content while `cb` is
+  * running asynchronously.
+  * @param {Element} parent
+  * @param {() => Promise<Element>} cb
+  * @returns {Promise<null>}
+  * @example
+  *   const parentElement = document.querySelector("#parent");
+  *   loadUIWithSpinner(parentElement, async () => {
+  *     const data = await (await fetch("/api/endpoint")).json();
+  *     const ul = document.createElement("ul");
+  *     for (const resource of data) {
+  *       const li = document.createElement("li");
+  *       li.textContent = resource;
+  *       ul.appendChild(li);
+  *     }
+  *     return ul;
+  *   })
   */
-export const onPageLoad = (callback) => {
+export const loadUIWithSpinner = async (parent, cb) => {
+  const spinner = document.createElement("img");
+  spinner.src = SPINNER_IMAGE_URL;
+  spinner.alt = "A loading spinner displayed while waiting for data to be fetched from the server.";
+  spinner.crossOrigin = "anonymous";
+  parent.innerHTML = "";
+  parent.appendChild(spinner);
+  const newNode = await cb();
+  spinner.replaceWith(newNode);
+}
+
+/** Replace child node tree of `parent` with `Element` returned by `cb`.
+  * @param {Element} parent
+  * @param {() => Promise<Element>} cb
+  * @returns {Promise<null>}
+  * @example
+  *   const parentElement = document.querySelector("#parent");
+  *   loadUI(parentElement, async () => {
+  *     const data = await (await fetch("/api/endpoint")).json();
+  *     const ul = document.createElement("ul");
+  *     for (const resource of data) {
+  *       const li = document.createElement("li");
+  *       li.textContent = resource;
+  *       ul.appendChild(li);
+  *     }
+  *     return ul;
+  *   })
+  */
+export const loadUI = async (parent, cb) => {
+  parent.innerHTML = "";
+  const newNode = await cb();
+  parent.appendChild(newNode);
+}
+
+
+/** Directly replace `placeholder` with `Element` returned by `cb`.
+  * Adds a loading spinner in place of the dynamic content while `cb` is
+  * running asynchronously.
+  * @param {Element} placeholder
+  * @param {() => Promise<Element>} cb
+  * @returns {Promise<null>}
+  * @example
+  *   const placeholderElement = document.querySelector("#placeholder");
+  *   replaceUIWithSpinner(placeholderElement, async () => {
+  *     const data = await (await fetch("/api/endpoint")).json();
+  *     const ul = document.createElement("ul");
+  *     for (const resource of data) {
+  *       const li = document.createElement("li");
+  *       li.textContent = resource;
+  *       ul.appendChild(li);
+  *     }
+  *     return ul;
+  *   })
+  */
+export const replaceUIWithSpinner = async (placeholder, cb) => {
+  const spinner = document.createElement("img");
+  spinner.src = SPINNER_IMAGE_URL;
+  spinner.alt = "A loading spinner displayed while waiting for data to be fetched from the server.";
+  spinner.crossOrigin = "anonymous";
+  placeholder.replaceWith(spinner);
+  const newNode = await cb();
+  spinner.replaceWith(newNode);
+}
+
+/** Directly replace `placeholder` with `Element` returned by `cb`.
+  * @param {Element} placeholder
+  * @param {() => Promise<Element>} cb
+  * @returns {Promise<null>}
+  * @example
+  *   const placeholderElement = document.querySelector("#placeholder");
+  *   replaceUI(placeholderElement, async () => {
+  *     const data = await (await fetch("/api/endpoint")).json();
+  *     const ul = document.createElement("ul");
+  *     for (const resource of data) {
+  *       const li = document.createElement("li");
+  *       li.textContent = resource;
+  *       ul.appendChild(li);
+  *     }
+  *     return ul;
+  *   })
+  */
+export const replaceUI = async (placeholder, cb) => {
+  const newNode = await cb();
+  placeholder.replaceWith(newNode);
+}
+
+
+
+/** Bind some JS code to run on page load.
+  * @param {() => null} cb A 0-arity function to call once the page content is loaded.
+  */
+export const onPageLoad = (cb) => {
   if (document.readyState !== 'loading') {
-    callback();
+    cb();
   } else {
     document.addEventListener('DOMContentLoaded', () => {
-      callback();
+      cb();
     })
   }
 }
